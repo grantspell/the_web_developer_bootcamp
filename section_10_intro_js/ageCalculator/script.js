27ce76bb2bcc3ab9a36dcf67770b93040dc5d741
grantspell <spellgrant@gmail.com>
2019-04-18 10:30:06 -0400

    Added some siple text to alert in ageCalculator to better explain the proccess

--- section_10_intro_js/ageCalculator/script.js
+++ section_10_intro_js/ageCalculator/script.js
@@ -1,3 +1,3 @@
 var userAgeInput = prompt("What is your age (whole #'s only, please)?")
 
-alert(userAgeInput * 365);
+alert("You are " + userAgeInput * 365 + " days old! Congratulations for making it this far!");
