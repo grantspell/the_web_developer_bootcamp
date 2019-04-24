13024511f54e536261e08cb3d9a5e8e5b7320db6
grantspell <spellgrant@gmail.com>
2019-04-24 11:45:24 -0400

    Completed annoyOmatic with while loop that checks input for correct answer, if not prompt continues, if correct the loop will end with a congratulations message.

--- section_11_js_control_flow/annoyOmatic/app.js
+++ section_11_js_control_flow/annoyOmatic/app.js
@@ -1,1 +1,8 @@
-alert("Connected");
+var userInput = "";
+
+while( userInput != "yes" || userInput != "yeah" ){
+    userInput = prompt("Are we there yet?")
+    if( userInput == "yes" || userInput == "yeah" ){
+        alert("Yay, we finally made it!")
+    };
+};
