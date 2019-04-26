d7114167b2ad93083e48497705603e61001ffe68
grantspell <spellgrant@gmail.com>
2019-04-26 11:33:05 -0400

    Completed Scope Code Along

---
+++ section_12_js_basics_functions/scope/app.js
@@ -0,0 +1,24 @@
+var x = "hello";
+
+function doMath() {
+    var x = 40;
+    console.log(x)
+};
+
+var y = 99;
+
+function doMoreMath() {
+    console.log(y);
+};
+
+function doMostMath() {
+    y = 100;
+    console.log(y)
+};
+
+var phrase = "hi there";
+
+function doSomething() {
+    var phrase = "goodbye";
+    console.log(phrase)
+}
