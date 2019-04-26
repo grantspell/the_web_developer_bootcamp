258b2497f50e997d406ad19a61dee40fae3ac67d
grantspell <spellgrant@gmail.com>
2019-04-26 11:53:36 -0400

    Completed JS Basics: Higher Order Functions code-along

---
+++ section_12_js_basics_functions/scope/hOF.js
@@ -0,0 +1,11 @@
+function sing() {
+    console.log("twinkle twinkle...");
+    console.log("how I wonder...")
+};
+
+setInterval(function(){
+    console.log("I am an anonymous function!");
+    console.log("THIS IS AWESOME!");
+}, 2000);
+
+// clearInterval(interval#);
