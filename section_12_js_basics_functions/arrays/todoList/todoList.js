b635665e5df2bb68e6b053c85680d346e776d45a
grantspell <spellgrant@gmail.com>
2019-04-26 14:58:50 -0400

    Completed Todo List Code Along Part II

--- section_12_js_basics_functions/arrays/todoList/todoList.js
+++ section_12_js_basics_functions/arrays/todoList/todoList.js
@@ -5,12 +5,33 @@ setTimeout(function () {
 
     while (input !== "quit") {
         if (input === "list") {
-            console.log(todos);
+            listTodos();
         } else if (input === "new") {
-            var newTodo = prompt("Enter new todo");
-            todos.push(newTodo);
+            newTodo();
+        } else if (input === "delete") {
+            deleteTodo();
         }
         input = prompt("What would you like to do?");
     }
     console.log("OK, YOU QUIT THE APP");
 }, 500);
+
+function listTodos() {
+    todos.forEach(function (todo, i) {
+        console.log("**********");
+        console.log(i + ". " + todo);
+        console.log("**********");
+    })
+};
+
+function newTodo() {
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+    console.log(newTodo + " was added to your todo list!")
+}
+
+function deleteTodo() {
+    var todoIndexD = prompt("Enter todo index #")
+    todos.splice(todoIndexD, 1);
+    console.log("Todo was deleted!")
+}
