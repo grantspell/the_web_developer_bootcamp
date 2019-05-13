b2db89fd82aa8cc1f1f0918108fa872cce108996
grantspell <spellgrant@gmail.com>
2019-05-13 12:14:53 -0400

    Created two game modes, easy and hard, with logic that generates new colors each time mode is changed and adjusts how many squares are represented in the DOM according to mode as well

--- section_17_color_game_project/colorGame.js
+++ section_17_color_game_project/colorGame.js
@@ -1,15 +1,46 @@
-var colors = generateRandomColors(6);
-
+var numOfSquares = 6;
+var colors = generateRandomColors(numOfSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numOfSquares = 3;
+    colors = generateRandomColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numOfSquares = 6;
+    colors = generateRandomColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+            squares[i].style.background = colors[i];
+            squares[i].style.display = "block";
+    }
+});
 
 resetButton.addEventListener("click", function(){
     // GENERATE ALL NEW COLORS
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numOfSquares);
     // PICK NEW RANDOM COLOR FROM ARRAY
     pickedColor = pickColor();
     // CHANGE colorDisplay TO MATCH pickedColor
