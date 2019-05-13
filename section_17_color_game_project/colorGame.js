c842ecc212f5271a280a87979b2dbd429c98ea7c
grantspell <spellgrant@gmail.com>
2019-05-13 11:42:07 -0400

    Completed random color selection for array of colors displayed as options, as well as added styling for h1 when color is guessed correctly

--- section_17_color_game_project/colorGame.js
+++ section_17_color_game_project/colorGame.js
@@ -1,9 +1,10 @@
-var colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(0, 0, 255)", "rgb(255, 0, 255)"]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +20,7 @@ for(var i = 0; i < squares.length; i++) {
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
@@ -38,3 +40,25 @@ function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
+
+function generateRandomColors(num){
+    // MAKE AN ARRAY
+    var arr = []
+    // REPEAT NUM TIMES
+    for(var i = 0; i < num; i++) {
+        // GET RANDOM COLOR, PUSH INTO ARRAY
+        arr.push(randomColor());
+    }
+    // RETURN THAT ARRAY
+    return arr;
+}
+
+function randomColor() {
+    // PICK A "RED" FROM 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    // PICK A "GREEN" FROM 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    // PICK A "BLUE" FROM 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+};
